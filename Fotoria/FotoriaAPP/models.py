45e46275ad6900ffd90f0bf512de4a29bd22a2b6
MauricioNLM <mauricio.navarro@sdgku.edu>
2025-02-03 19:51:47 -0800

    version 1.0.0

--- Fotoria/FotoriaAPP/models.py
+++ Fotoria/FotoriaAPP/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Picture(models.Model):
+    name = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    date = models.DateField()
+    location = models.CharField(max_length=100)
+    urlimg = models.URLField()
+
+    def __str__(self):
+        return self.name
--- Fotoria/FotoriaAPP/views.py
+++ Fotoria/FotoriaAPP/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from . import models, forms
 
 def home(request):
-    return HttpResponse("¡Hola, mundo desde Django!")
+
+    pictureList = models.Picture.objects.all()
+
+    return render(request,'home.html',{'listPicture': pictureList})
+
+def newPicture(request):
+
+    if request.method == 'POST':
+        form = forms.PictureForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = forms.PictureForm()
+
+    return render(request, 'newPicture.html', {'form': form})
 
 # Create your views here.
