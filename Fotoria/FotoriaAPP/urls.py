8ae3239e685ca0de43cf85a4c09133eb6940e970
MauricioANL <Mauricio.Navarro@sdgku.edu>
2025-02-12 19:41:32 -0800

    versión 2.1.0

--- Fotoria/FotoriaAPP/urls.py
+++ Fotoria/FotoriaAPP/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.shortcuts import redirect
 from django.contrib.auth import views as auth_views
-from .views import PictureListView, PictureCreateView, UserRegisterView
+from .views import PictureListView, PictureCreateView, UserRegisterView, PictureUpdateView
 
 urlpatterns = [
     path('', lambda request: redirect('login')),
@@ -11,4 +11,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', UserRegisterView.as_view(), name='register'),
+    path('edit/<int:pk>/', PictureUpdateView.as_view(), name='editPicture'),
+
 ]
